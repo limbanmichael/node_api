b53978b448bb0fdbc5dc96bd04dddfa1f67107b5
michaell@agtechlabs.com <michaell@agtechlabs.com>
2017-12-23 14:39:28 +0800

    port number changed

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use(function(err, req, res, next){
 });
 
 // listen for request
-app.listen(process.env.port || 4000, function (){
+app.listen(process.env.port || port, function (){
   console.log('now listening for request');
 });
