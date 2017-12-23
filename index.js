fc6368fd26809e0b4a4e3b149a346f921f4742c1
michaell@agtechlabs.com <michaell@agtechlabs.com>
2017-12-23 16:47:37 +0800

    connect mongodb_uri

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // connect to mongodb
-mongoose.connect('mongodb://localhost/ninjago', {
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true,
 });
 mongoose.Promise = global.Promise;
